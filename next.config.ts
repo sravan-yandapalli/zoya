55571978e4a4b71fd0cf8846a40d848dd527a6b7
sravan-yandapalli <yandapallisravankumar@gmail.com>
2025-01-31 00:55:43 +0530

    installing amplify

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Enables static export for GitHub Pages
   images: {
-    unoptimized: true, // Prevents Next.js from optimizing images (required for GitHub Pages)
+    unoptimized: true, // Disable Next.js image optimization (if needed for your use case)
   },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/zoya" : "", // Ensures assets are correctly referenced
-  basePath: process.env.NODE_ENV === "production" ? "/zoya" : "", // Fixes routing for GitHub Pages
 };
 
 module.exports = nextConfig;
